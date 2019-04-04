579618941c9a45aee391afbc2233f183e3d18902
gbarrbrown <gbb7899@gmail.com>
2019-04-04 22:32:01 +1300

    webpack & babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,35 @@
-module.exports = {
-    module: {
-        rules: [
-            {
-                rest: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
         ]
-    }
-};
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ]
+  }
+}
+
+module.exports = config;
